b9faa63bdca41db914ffbe04d58bcda12201fb15
Pouya <pouyamohammadyirbu@gmail.com>
2024-06-03 10:39:04 +0330

    init front base structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Grid
+      templateAreas={{
+        base: `"nav nav" "tools main"`,
+        lg: `"nav nav nav" "tools main props"`,
+      }}
+      templateColumns={{
+        base: "200px 1fr",
+        lg: "250px 1fr 250px",
+      }}
+    >
+      <GridItem area="nav" bgColor={"red"}>
+        Nav
+      </GridItem>
+
+      <GridItem area="tools" bgColor={"black"}>
+        Components
+      </GridItem>
+
+      <GridItem area="main" bgColor={"orange"}>
+        Main
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="props" bgColor={"blue"}>
+          Props
+        </GridItem>
+      </Show>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
