753a0e0ea4a4abd5b07ebb1a16c631a827b6a147
Pouya <pouyamohammadyirbu@gmail.com>
2024-06-04 17:49:30 +0330

    building dropdown with animation

--- frontend/src/components/DropDownMenu.tsx
+++ frontend/src/components/DropDownMenu.tsx
@@ -1,20 +1,32 @@
 import { Box, Button, Menu, MenuItem } from "@chakra-ui/react";
 import "../style/drop-down-menu.css";
+import { useState } from "react";
 
 interface Props {
   title: String;
 }
 
 const DropDownMenu = ({ title }: Props) => {
+  const [isOpen, setIsOpen] = useState<Boolean>(false);
+
   return (
     <div className="drop-down-menu">
-      <Button className="drop-btn dropdown-toggle">{title}</Button>
-      <Box className="expandable-menu">
+      <Button
+        className="drop-btn dropdown-toggle"
+        onClick={() => {
+          setIsOpen(isOpen ? false : true);
+        }}
+      >
+        {title}
+      </Button>
+      <Box
+        className={"expandable-menu " + (isOpen ? "drop-open" : "drop-close")}
+      >
         <Menu>
           <MenuItem className="menu-item">Item 1</MenuItem>
-          <MenuItem>Item 2</MenuItem>
-          <MenuItem>Item 3</MenuItem>
-          <MenuItem>Item 4</MenuItem>
+          <MenuItem className="menu-item">Item 2</MenuItem>
+          <MenuItem className="menu-item">Item 3</MenuItem>
+          <MenuItem className="menu-item">Item 4</MenuItem>
         </Menu>
       </Box>
     </div>
