cd3a4c3cdac9031af0b0b9d94d2b96d1c633d195
Pouya <pouyamohammadyirbu@gmail.com>
2024-10-10 15:15:33 +0330

    update

--- frontend/src/__test__/App.test.jsx
+++ frontend/src/__test__/App.test.jsx
@@ -1,6 +1,5 @@
 import App from "../App";
-import { render } from "@testing-library/react";
-import "@testing-library/jest-dom";
+import { render, screen, fireEvent } from "@testing-library/react";
 
 test("demo", () => {
   expect("true").toBe("true");
--- frontend/src/modules.js
+++ frontend/src/modules.js
@@ -190,7 +190,8 @@ const Descriptions = {
     "In statistics, the k-nearest neighbors algorithm is a non-parametric supervised learning method first developed by Evelyn Fix and Joseph Hodges in 1951, and later expanded by Thomas Cover. It is used for classification and regression. In both cases, the input consists of the k closest training examples in a data set.",
   "line-graph-des":
     "In the mathematical discipline of graph theory, the line graph of an undirected graph G is another graph L(G) that represents the adjacencies between edges of G. L(G) is constructed in the following way: for each edge in G, make a vertex in L(G); for every two edges in G that have a vertex in common, make an edge between their corresponding vertices in L(G).",
-  "decision-boundary-plot-des": "A decision boundary/surface is a plot that shows how a trained model makes its predictions according to a feature space.",
+  "decision-boundary-plot-des":
+    "A decision boundary/surface is a plot that shows how a trained model makes its predictions according to a feature space.",
 };
 
 export const getDescriptions = (href) => {
