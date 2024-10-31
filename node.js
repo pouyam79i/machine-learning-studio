23c8269a649686b5e736e495c60ebe15a3117791
Pouya <pouyamohammadyirbu@gmail.com>
2024-10-31 10:41:47 +0330

    update

---
+++ node.js
@@ -0,0 +1,28 @@
+const node = 
+
+{
+  id: "466d860c-9620-4444-988a-4bbd59b8dc42",
+  type: "SimpleNode",
+  position: {
+    x: -105,
+    y: -60,
+  },
+  data: {
+    title: "K-NN",
+    status: "inactive",
+    tag: "/ml/knn",
+    post: "inner",
+    props: [
+      {
+        id: 0,
+        title: "Number of neighbors...",
+        tag: "neighbors",
+        type: "text-field",
+        data: "3",
+      },
+    ],
+    label: "",
+  },
+};
+
+
